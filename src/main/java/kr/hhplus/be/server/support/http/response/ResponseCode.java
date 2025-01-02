4a755d6fd3325d1df43f01de2f5c1ddcf9133dab
nimoh <spakers38@gmail.com>
2025-01-03 01:14:32 +0900

    feat: 응답 코드 재정의

--- src/main/java/kr/hhplus/be/server/support/http/response/ResponseCode.java
+++ src/main/java/kr/hhplus/be/server/support/http/response/ResponseCode.java
@@ -7,26 +7,32 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ResponseCode {
 
-    // 10XX: 포인트
-    SUCCESS_CHARGE_POINT("1000", "포인트 충전 성공")
-    , SUCCESS_SEARCH_USER_POINT("1001", "사용자 포인트 조회 성공")
+    // 2XXX: 성공
 
-    // 11XX: 상품
-    , SUCCESS_SEARCH_PRODUCTS("1100", "상품 조회 성공")
+        // 20XX: 포인트
+        SUCCESS_CHARGE_POINT("2000", "포인트 충전 성공")
+        , SUCCESS_SEARCH_USER_POINT("2001", "사용자 포인트 조회 성공")
 
-    // 12XX: 쿠폰
-    , SUCCESS_ISSUE_COUPON("1200", "쿠폰 발급 성공")
-    , SUCCESS_SEARCH_USER_COUPON("1201", "사용자 쿠폰 목록 조회 성공")
-    , SUCCESS_SEARCH_AVAILABLE_COUPON("1202", "발급 가능한 쿠폰 목록 조회 성공")
+        // 21XX: 상품
+        , SUCCESS_SEARCH_PRODUCTS("2100", "상품 조회 성공")
 
-    // 13XX: 주문
-    , SUCCESS_ORDER("1300", "주문 성공")
-    , SUCCESS_SEARCH_ORDERS("1301", "주문 목록 조회")
-    , SUCCESS_SEARCH_TOP_ORDERS("1302", "상위 주문 상품 조회")
+        // 22XX: 쿠폰
+        , SUCCESS_ISSUE_COUPON("2200", "쿠폰 발급 성공")
+        , SUCCESS_SEARCH_USER_COUPON("2201", "사용자 쿠폰 목록 조회 성공")
+        , SUCCESS_SEARCH_AVAILABLE_COUPON("2202", "발급 가능한 쿠폰 목록 조회 성공")
 
-    // 14XX: 결제
-    , SUCCESS_PAYMENT("1400", "결제 성공")
-    , SUCCESS_SEARCH_PAYMENT("1401", "사용자별 결제 내역 조회")
+        // 23XX: 주문
+        , SUCCESS_ORDER("2300", "주문 성공")
+        , SUCCESS_SEARCH_ORDERS("2301", "주문 목록 조회")
+        , SUCCESS_SEARCH_TOP_ORDERS("2302", "상위 주문 상품 조회")
+
+        // 24XX: 결제
+        , SUCCESS_PAYMENT("2400", "결제 성공")
+        , SUCCESS_SEARCH_PAYMENT("2401", "사용자별 결제 내역 조회")
+
+    // 4XXX: 클라이언트 에러
+
+    // 5XXX: 서버 에라
     ;
 
     private final String code;
