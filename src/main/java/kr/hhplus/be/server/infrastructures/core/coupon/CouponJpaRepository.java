b0cf5b8e330a219ee451e4fc2a31c3ed9cf0f7be
nimoh <spakers38@gmail.com>
2025-01-10 01:34:15 +0900

    fix: Jpa Repository를 스캔하지 못하는 이슈 수

--- src/main/java/kr/hhplus/be/server/infrastructures/core/coupon/CouponJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructures/core/coupon/CouponJpaRepository.java
@@ -2,12 +2,6 @@ package kr.hhplus.be.server.infrastructures.core.coupon;
 
 import kr.hhplus.be.server.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
-
-    @Query("select c from Coupon c join UserCoupon uc on c.id = uc.couponId")
-    List<Coupon> findByUserCouponId(Long userId);
 }
