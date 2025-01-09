29a9b3ae37b560f135fe612672e53fe03c9dccb1
nimoh <spakers38@gmail.com>
2025-01-09 17:43:05 +0900

    feat: 쿠폰 관련 예외클래스 생성

---
+++ src/main/java/kr/hhplus/be/server/domain/coupon/exception/CouponNotFoundException.java
@@ -0,0 +1,4 @@
+package kr.hhplus.be.server.domain.coupon.exception;
+
+public class CouponNotFoundException extends RuntimeException {
+}
---
+++ src/main/java/kr/hhplus/be/server/domain/coupon/exception/CouponStockNotEnoughException.java
@@ -0,0 +1,4 @@
+package kr.hhplus.be.server.domain.coupon.exception;
+
+public class CouponStockNotEnoughException extends RuntimeException {
+}
