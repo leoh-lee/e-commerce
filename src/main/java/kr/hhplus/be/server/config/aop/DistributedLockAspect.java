b22e8edec38f600e2efb79533cda0c88fd0cc3b7
nimoh <spakers38@gmail.com>
2025-02-01 16:48:40 +0900

    feat: Redisson을 이용한 분산락 환경 세

---
+++ src/main/java/kr/hhplus/be/server/config/aop/DistributedLockAspect.java
@@ -0,0 +1,62 @@
+package kr.hhplus.be.server.config.aop;
+
+import kr.hhplus.be.server.config.annotation.DistributedLock;
+import kr.hhplus.be.server.support.util.CustomSpringELParser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class DistributedLockAspect {
+
+    private static final String REDISSON_LOCK_PREFIX = "LOCK:";
+
+    private final RedissonClient redissonClient;
+
+    @Around("@annotation(kr.hhplus.be.server.config.annotation.DistributedLock)")
+    public Object lock(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
+
+        String key = REDISSON_LOCK_PREFIX + CustomSpringELParser.getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), distributedLock.key());
+
+        log.info("{}#{} | Try to get DistributedLock. Key >>> {}", method.getDeclaringClass().getSimpleName(), method.getName(), key);
+
+        RLock rLock = redissonClient.getLock(key);
+        try {
+            boolean available = rLock.tryLock(distributedLock.waitTime(), distributedLock.leaseTime(), distributedLock.timeUnit());
+            if (!available) {
+                log.info("{}#{} | Fail to get DistributedLock. Key >>> {}", method.getDeclaringClass().getSimpleName(), method.getName(), key);
+                return false;
+            }
+
+            log.info("{}#{} | Success to get DistributedLock. Key >>> {}", method.getDeclaringClass().getSimpleName(), method.getName(), key);
+
+            return joinPoint.proceed();
+        } catch (InterruptedException e) {
+            throw new InterruptedException();
+        } finally {
+            try {
+                rLock.unlock();
+                log.info("DistributedLock is unlocked. Key is {}", key);
+            } catch (IllegalMonitorStateException e) {
+                log.info("Redisson Lock Already UnLock serviceName: {} key: {}", method.getName(), key);
+            }
+        }
+    }
+}
